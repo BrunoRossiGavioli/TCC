072fd77c258aada20afab8df83c42db9d6f16dfd
Bruno Rossi <bruno.gavioli@etec.sp.gov.br>
2021-04-17 16:02:23 -0300

    Revert "Delete branch Layout"
    This reverts commit f30f453376c65f783eeea3faef2a158b45ecf8d0.

---
+++ TCCESTOQUE/Interfaces/Service/IProdutoService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TCCESTOQUE.Models;
+
+namespace TCCESTOQUE.Interfaces.Service
+{
+    public interface IProdutoService
+    {
+        public object GetIndex();
+
+        public ProdutoModel GetDetalhes(int? id);
+
+        public object GetCriacao();
+
+        public object PostCriacao(ProdutoModel produtoModel);
+
+        public ProdutoModel GetEdicao(int? id);
+
+        public object PutEdicao(int id, ProdutoModel produtoModel);
+
+        public ProdutoModel GetExclusao(int? id);
+
+        public object PostExclusao(int id);
+    }
+}
---
+++ TCCESTOQUE/Validacao/Formatacao/FormataValores.cs
@@ -0,0 +1,20 @@
+using System;
+using TCCESTOQUE.Models;
+using TCCESTOQUE.Service;
+
+namespace TCCESTOQUE.Validacao.Formatacao
+{
+    public class FormataValores
+    {
+        public static VendedorModel FormataValoresVendedor(VendedorModel vendedorModel)
+        {
+            vendedorModel.Nome = vendedorModel.Nome.ToUpper().Trim();
+            vendedorModel.Endereco = vendedorModel.Endereco.ToUpper().Trim();
+            vendedorModel.Cpf = vendedorModel.Cpf.Trim();
+            vendedorModel.Telefone = vendedorModel.Telefone.Trim();
+            vendedorModel.Email = vendedorModel.Email.Trim();
+            vendedorModel.Senha = SecurityService.Criptografar(vendedorModel.Senha);
+            return vendedorModel;
+        }
+    }
+}
