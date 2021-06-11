26ce3e84bc052a2a8d45879eabfc2e0e9c295555
BrunoRG <bruno.gavioli@etec.sp.gov.br>
2021-06-10 23:33:02 -0300

    Deixando layout bunitu

--- TCCESTOQUE/ViewModel/ProdutoViewModel.cs
+++ TCCESTOQUE/ViewModel/ProdutoViewModel.cs
@@ -8,17 +8,18 @@ namespace TCCESTOQUE.ViewModel
     {
         public Guid VendedorId { get; set; }
         public Guid FornecedorId { get; set; }
-        [Required(ErrorMessage = "Insira o Nome", AllowEmptyStrings = false)]
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        [Required(ErrorMessage = "Insira o Custo", AllowEmptyStrings = false)]
-        public decimal Custo { get; set; }
-        [Required(ErrorMessage = "Insira o Valor", AllowEmptyStrings = false)]
-        public decimal Valor { get; set; }
-        [Required(ErrorMessage = "Insira a Quantidade", AllowEmptyStrings = false)]
 
+        [Required(ErrorMessage = "Insira o custo", AllowEmptyStrings = false)]
+        public decimal Custo { get; set; }
+        
+        [Required(ErrorMessage = "Insira o valor", AllowEmptyStrings = false)]
+        public decimal Valor { get; set; }
+        
+        [Required(ErrorMessage = "Insira a quantidade", AllowEmptyStrings = false)]
         public double Quantidade { get; set; }
-        [Required(ErrorMessage = "Insira a Unidade de Medida", AllowEmptyStrings = false)]
+        
         public UnidadeDeMedidaEnum UnidadeMedida { get; set; }
     }
 }
